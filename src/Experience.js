5f7ecb570d23caa4e04be818edebee6a552df6c7
Codingisfun190 <chanyoung720@gmail.com>
2023-02-19 15:19:20 -0500

    changes

---
+++ src/Experience.js
@@ -0,0 +1,39 @@
+const Experience = () => {
+  return (
+    <div className="Experience">
+      <div className="exp">
+        <p>
+          <li>2006 - 2020 : Regency Cleaners Manager</li>
+        </p>
+      </div>
+      <p>
+        <li>
+          2010 - 2014 : Applebees carside associate, host, server, dishwasher,
+          fryside cook, barback, food prep, food expo
+        </li>
+      </p>
+      <p>
+        <li>2010 - 2017 : Rite-Aid Shift Supervisor, Assistant Manager</li>
+      </p>
+      <p>
+        <li>
+          2017 - 2021 : Walgreens Assistant Manager, Acting General Manager
+        </li>
+      </p>
+      <p>
+        <li>
+          2021 - present : Learning to code, specifically front end web
+          development, such as HTML, CSS, Javascript and React
+        </li>
+      </p>
+      <p>
+        <li>
+          June 2022 - August 2022 : Successful completion of General Assembly
+          front end web development bootcamp
+        </li>
+      </p>
+    </div>
+  )
+}
+
+export default Experience
---
+++ src/Portfolio.js
@@ -0,0 +1,71 @@
+const Portfolio = () => {
+  return (
+    <div className="Portfolio">
+      <div>
+        <h2>Portfolio</h2>
+      </div>
+      <div className="gallery">
+        <div className="projects">
+          <div className="box">
+            <a
+              href="https://codingisfun190.github.io/EarthTribe/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="img-fit"
+                src="/images/Earthtribe-thumbnail.PNG"
+                alt="EarthTribe Project"
+              />
+            </a>
+          </div>
+          <div className="box">
+            <a
+              href="https://codingisfun190.github.io/Marvel_movies/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="img-fit"
+                src="/images/Marvel-thumbnail.PNG"
+                alt="EarthTribe Project"
+              />
+            </a>
+          </div>
+          <div className="box">
+            <a
+              href="https://codingisfun190.github.io/Dots_Project/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="img-fit"
+                src="/images/Dots-thumbnail.PNG"
+                alt="EarthTribe Project"
+              />
+            </a>
+          </div>
+          <div className="box">
+            <a
+              href="https://codingisfun190.github.io/Dots_Project/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="img-fit"
+                src="/images/Dots-thumbnail.PNG"
+                alt="EarthTribe Project"
+              />
+            </a>
+          </div>
+          <div className="box">fifth project</div>
+          <div className="box">sixth project</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Portfolio
+
+/* <a class="link" href="https://github.com/dolaku/TriviaGame" target="_blank" rel="noopener noreferrer">…</a><a href="https://dolaku.github.io/TriviaGame/" target="_blank">…</a> */
